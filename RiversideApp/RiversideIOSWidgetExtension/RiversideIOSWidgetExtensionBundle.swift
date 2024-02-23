76abf5ce2197d231f6a5582bd3951031f3ede2ad
maiyama <myam0118@gmail.com>
2024-02-23 13:45:05 +0900

    Add widget extension

---
+++ RiversideApp/RiversideIOSWidgetExtension/RiversideIOSWidgetExtensionBundle.swift
@@ -0,0 +1,17 @@
+//
+//  RiversideIOSWidgetExtensionBundle.swift
+//  RiversideIOSWidgetExtensionExtension
+//
+//  Created by maiyama18 on 2024/02/23
+//  
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RiversideIOSWidgetExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        RiversideIOSWidgetExtension()
+    }
+}
