3e424a4e5529f6a210c9a352ac34192c179ec51f
maiyama <myam0118@gmail.com>
2024-07-21 10:53:53 +0900

    Read port from environment

--- Server/RiversideServer/Sources/App/configure.swift
+++ Server/RiversideServer/Sources/App/configure.swift
@@ -1,5 +1,6 @@
 import Vapor
 
 public func configure(_ app: Application) async throws {
+    app.http.server.configuration.port = Environment.get("PORT").flatMap(Int.init) ?? 8080
     try routes(app)
 }
