1bc1a990cfafa6f13443b0b1b4df89f3ecad330e
maiyama <myam0118@gmail.com>
2024-08-03 14:35:50 +0900

    Use client timeout

--- Server/RiversideServer/Sources/App/configure.swift
+++ Server/RiversideServer/Sources/App/configure.swift
@@ -5,6 +5,8 @@ import FluentSQLiteDriver
 public func configure(_ app: Application) async throws {
     app.http.server.configuration.port = Environment.get("PORT").flatMap(Int.init) ?? 8080
     
+    app.http.client.configuration.timeout = .init(read: .seconds(15))
+    
     app.databases.use(.sqlite(.file("db.sqlite3")), as: .sqlite)
     app.migrations.add(CreateFeed(), to: .sqlite)
     app.migrations.add(CreateEntry(), to: .sqlite)
